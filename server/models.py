d6f16b93977909b29d289c859c62d1f1c659e512
Marvin Kabira <marvobd@gmail.com>
2024-08-29 17:10:08 +0300

    completed lab

--- server/models.py
+++ server/models.py
@@ -1,21 +1,29 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from models import db, Pet
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
+db.init_app(app)
 
-# create the Flask SQLAlchemy extension
-db = SQLAlchemy(metadata=metadata)
+@app.route('/')
+def index():
+    return 'Welcome to the Pet API!'
 
-# define a model class by inheriting from db.Model.
+@app.route('/pets', methods=['POST'])
+def create_pet():
+    data = request.json
+    new_pet = Pet(name=data['name'], species=data['species'])
+    db.session.add(new_pet)
+    db.session.commit()
+    return jsonify(new_pet.id), 201
 
+@app.route('/pets', methods=['GET'])
+def get_pets():
+    pets = Pet.query.all()
+    return jsonify([{'id': pet.id, 'name': pet.name, 'species': pet.species} for pet in pets])
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
-
-    def __repr__(self):
-        return f'<Pet {self.id}, {self.name}, {self.species}>'
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
